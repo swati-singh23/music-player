4e5313de676250fb943f386c303f6ec5d51e1cb4
swati-singh23 <139169646+swati-singh23@users.noreply.github.com>
2023-12-03 00:26:56 +0530

    added

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+function addSong() {
+  const playlist = document.getElementById('playlist');
+  const songNameInput = document.getElementById('songName');
+  const songName = songNameInput.value.trim();
+
+  if (songName !== '') {
+    const li = document.createElement('li');
+    li.className = 'song';
+    li.innerHTML = `
+      <span>${songName}</span>
+      <div class="song-actions">
+        <button onclick="removeSong(this)">Remove</button>
+        <button onclick="moveUp(this)">Move Up</button>
+        <button onclick="moveDown(this)">Move Down</button>
+      </div>
+    `;
+    playlist.appendChild(li);
+    songNameInput.value = '';
+  }
+}
+
+function removeSong(button) {
+  const li = button.closest('.song');
+  li.remove();
+}
+
+function moveUp(button) {
+  const li = button.closest('.song');
+  const previousLi = li.previousElementSibling;
+
+  if (previousLi) {
+    li.parentNode.insertBefore(li, previousLi);
+  }
+}
+
+function moveDown(button) {
+  const li = button.closest('.song');
+  const nextLi = li.nextElementSibling;
+
+  if (nextLi) {
+    li.parentNode.insertBefore(nextLi, li);
+  }
+}
+
+
+
 var audio = document.getElementById("myAudio");
 var playPauseBtn = document.getElementById("playPauseBtn");
 var prevBtn = document.getElementById("prevBtn");
